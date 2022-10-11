ce3b5d3ae0bcde56992f9f8f2b37164c8a87b470
Marcos Paulo <itsolutions031250@gmail.com>
2022-10-11 16:41:05 -0300

    tryCatch

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,24 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("invalid position");
+        }
+
+        System.out.println("End of program");
+
+
+        sc.close();
     }
 }
