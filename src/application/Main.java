689ae7db698eb0b06e4c7db4c7282c58afbe1424
Marcos Paulo <itsolutions031250@gmail.com>
2022-10-14 10:04:13 -0300

    printStackTrace

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,29 +1,43 @@
 package application;
 
 import java.util.InputMismatchException;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
+
+        method1();
+
+        System.out.println("End of program");
+    }
+
+
+    public static void method1() {
+        System.out.println("method 1 start");
+        method2();
+        System.out.println("method 1 finish");
+    }
+
+    public static void method2(){
+        System.out.println("method 2 start");
         Scanner sc = new Scanner(System.in);
 
         try {
             String[] vect = sc.nextLine().split(" ");
             int position = sc.nextInt();
             System.out.println(vect[position]);
-        }
-        catch (ArrayIndexOutOfBoundsException e) {
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("invalid position");
-        }
-        catch (InputMismatchException e){
+            e.printStackTrace();
+            sc.next();
+        } catch (InputMismatchException e) {
             System.out.println("input error");
         }
 
         System.out.println("End of program");
 
-
         sc.close();
+
+        System.out.println("method 2 finish");
     }
 }
